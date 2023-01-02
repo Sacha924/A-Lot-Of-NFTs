2800c0ea46c638577083c9fbf144089874d05dfa
Sacha924 <sacha.simon@edu.devinci.fr>
2023-01-02 15:46:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+I will make 3 contract for 3 different NFT :
+
+1. A basic NFT using ERC721 standard
+2. Random IPFS hosted NFT
+3. Dynamic SVG NFT
+
+## Commands for Dependencies
+
+```
+yarn add --dev hardhat
+
+yarn hardhat
+
+yarn add --dev @typechain/ethers-v5 @typechain/hardhat @types/chai @types/node @types/mocha ts-node typechain typescript
+
+yarn add --dev @openzeppelin/contracts
+```
---
+++ contracts/basicNFT.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract basicNFT is ERC721 {
+    // function balanceOf(owner)  public override {}
+
+    // function ownerOf(tokenId) public override {}
+
+    // function safeTransferFrom(from, to, tokenId) public override {}
+
+    // function transferFrom(from, to, tokenId) public override {}
+
+    // function approve(to, tokenId) public override {}
+
+    // function getApproved(tokenId) public override {}
+
+    // function setApprovalForAll(operator, _approved) public override {}
+
+    // function isApprovedForAll(owner, operator) public override {}
+
+    // function safeTransferFrom(from, to, tokenId, data) public override {}
+}
